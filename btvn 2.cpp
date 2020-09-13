07f48f634a279d9fd8cbaba02185554eca282f10
doanmhinhduc <70931541+doanmhinhduc@users.noreply.github.com>
2020-09-13 23:14:25 +0700

    Add files via upload

---
+++ btvn 2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int a,b;
+	printf("nhap a=");
+	scanf("%d",&a);
+	printf("nhap b=");
+	scanf("%d",&b);
+	
+	
+	if(a>=b){
+		int c=a/b;
+		printf("%d ket qua",c);
+		
+	}else{
+		int c=a*b;
+		printf("%d ket qua",c);
+			}
+	}
---
+++ btvn1.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int n;
+	printf("nhap so bat ky:");
+	scanf("%d",&n);
+	
+	if(n>=2){
+		}else{
+		printf("khong phai la ngay trong tuan");}
+		if(n<=7){
+			printf("ngay thu: %d",n);
+		
+	}else{
+		printf("khong phai la ngay trong tuan");}
+		{
+		if(n==8){
+			printf("ngay chu nhat");
+		}else{
+			printf("khong phai la ngay trong tuan");
+		}
+		}
+	}
+} 
+	
+
